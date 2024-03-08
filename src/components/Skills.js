17287b836fe0738a0e0404e8654ecceaca22ba23
Dexxark <etcheverry.mathieu@gmail.com>
2024-03-08 09:35:24 +0100

    changes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import skillsJSON from "../data/skills.json";
 import { motion } from "framer-motion";
 
@@ -16,7 +15,7 @@ const Skills = () => {
   };
 
   return (
-    <section id="skills" className=" m-auto -my-20 bg-mygray px-5 py-14 brightness-[1.5] md:py-20 lg:-my-24">
+    <section id="skills" className="m-auto -my-20 bg-mygray px-5 py-14 brightness-[1.5] md:py-20 lg:-my-24">
       <h2 className="mb-[30px] mt-4 text-center text-white text-[2.5rem] blur-[0.5px] lg:text-[2.8rem]">
         Mes compétences
       </h2>
@@ -24,9 +23,9 @@ const Skills = () => {
         <div className="mb-5 mt-7 flex w-full flex-wrap justify-center gap-x-5 gap-y-5 pb-16 md:justify-between md:border-b-0 md:pr-6 md:pt-12 lg:gap-x-7">
           {skillsJSON.map((data, id) => (
             <motion.div
-            variants={animation}
+              variants={animation}
               key={id}
-              className=" group h-[72px] w-[72px] bg-transparent [perspective:1000px]"
+              className="group h-[72px] w-[72px]"
               whileInView="animate"
               custom={id}
               initial={{ opacity: 0, y: 100 }}
@@ -35,7 +34,7 @@ const Skills = () => {
             >
               <div className="h-full w-full text-center">
                 <div>
-                  <Image src={data.img} alt={`Icone de ${data.title}`} width={75} height={75}/>
+                  <img src={data.img} alt={`Icone de ${data.title}`} className="w-75 h-75"/>
                 </div>
               </div>
             </motion.div>
